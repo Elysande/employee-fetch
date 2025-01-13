2fb200717060637682cfdbd88cf5b5fe20b5a781
Elysande <sirinan@hotmail.fr>
2025-01-13 12:03:13 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import EmployeeCard from "./Components/EmployeeCard.tsx";
+import "./App.css";
+import { useState } from "react";
+
+const sampleEmployee = {
+	name: {
+		first: "Charlie",
+		last: "Thompson",
+	},
+	email: "charlie.thompson@example.com",
+	picture: {
+		medium: "https://randomuser.me/api/portraits/med/men/40.jpg",
+	},
+};
+
+function App() {
+	const [employee, setEmployee] = useState(sampleEmployee);
+
+	const getEmployee = () => {
+		fetch("http://localhost:3310/api/employees")
+			.then((response) => response.json())
+			.then((data) => {
+				setEmployee(data.results[0]);
+			});
+	};
+
+	return (
+		<div className="App">
+			<EmployeeCard employee={employee} />
+			<button type="button" onClick={getEmployee}>
+				Get employee
+			</button>
+		</div>
+	);
+}
+
+export default App;
